bae6751b06246bd775467a571252d96c27511f95
Daviid Valentino Bernth <davi9400@edu.eal.dk>
2019-05-01 10:16:12 +0100

    Added database class

---
+++ Domain/Database.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Database
+    {
+        private static string connectionString =
+            "Data Source = EALSQL1.Eal.Local;" +
+            "Database = C_DB10_2018;" +
+            "User ID = C_STUDENT10;" +
+            "Password = C_OPENDB10;";
+
+    }
+}
