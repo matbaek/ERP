70a2b1886e097efe68c8b4a9ee514cdabd0a9b0e
Mathias Bærentsen <mathias.barentsen@gmail.com>
2019-05-10 13:35:59 +0200

    Delete order

---
+++ ERP/Customer/WindowDeleteOrder.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Application;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ERP.Customer
+{
+    /// <summary>
+    /// Interaction logic for WindowDeleteOrder.xaml
+    /// </summary>
+    public partial class WindowDeleteOrder : Window
+    {
+        private OrderRepository orderRepository = new OrderRepository();
+        private Order order;
+        public WindowDeleteOrder(Order order)
+        {
+            InitializeComponent();
+            this.order = order;
+            LabelDeleteOrder.Content = $"Er du sikker på at du vil slette ordren '{order.OrderID}'?";
+        }
+
+        private void ButtonYes_Click(object sender, RoutedEventArgs e)
+        {
+            orderRepository.DeleteOrder(order.OrderID);
+            this.Close();
+        }
+
+        private void ButtonNo_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
